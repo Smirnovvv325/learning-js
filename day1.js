786127b351453a0663802dcc18b8ef6323762135
Smirnovvv325 <ivan32539764r@yandex.ru>
2026-06-13 03:02:22 +0300

    day1: js basics

---
+++ day1.js
@@ -0,0 +1,6 @@
+let name = "Ivan";
+let age = 21;
+let isIvanLoh = 1<0;
+let ageOfDeath = undefined;
+let rich = null
+console.log(typeof(name), typeof(age), typeof(isIvanLoh), typeof(rich), typeof(ageOfDeath), `меня зовут ${name}, мне ${age} год`);
